e75aeaa54f472383f79bd5d2f8aec5818be9fc15
julian-medve <julian.medve.superdev@gmail.com>
2020-07-15 21:28:22 +0900

    Design updated.

--- XMLMapper/Resource.h
+++ XMLMapper/Resource.h
@@ -2,20 +2,34 @@
 // Microsoft Visual C++ generated include file.
 // Used by XMLMapper.rc
 //
-#define IDR_MAINFRAME					128
-#define IDM_ABOUTBOX					0x0010
-#define IDD_ABOUTBOX					100
-#define IDS_ABOUTBOX					101
-#define IDD_XMLMAPPER_DIALOG				102
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_XMLMAPPER_DIALOG            102
+#define IDR_MAINFRAME                   128
+#define IDC_LIST2                       1001
+#define IDC_LIST3                       1002
+#define IDC_STATIC_MS_LAT               1003
+#define IDC_STATIC_X_LONG               1004
+#define IDC_STATIC_X_HEAD               1005
+#define IDC_STATIC_X_LAT                1006
+#define IDC_STATIC_MS_LONG              1007
+#define IDC_STATIC_MS_HEADING2          1008
+#define IDC_STATIC_MS_HEAD              1008
+#define IDC_STATIC_MS_LATITUDE          1009
+#define IDC_STATIC_X_LATITUDE           1010
+#define IDC_STATIC_X_LONGTITUDE         1011
+#define IDC_STATIC_X_HEADING            1012
+#define IDC_STATIC_MS_LONGTITUDE        1013
+#define IDC_STATIC_MS_HEADING           1014
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1015
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
